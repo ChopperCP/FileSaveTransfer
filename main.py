bb7a9efa9cee86b5da4a853acf786ce7a935f6fc
ChopperCP <568624486@qq.com>
2020-12-14 22:32:49 +0800

    Implemented the file transfer system

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-# from aes128 import *
+from aes128 import aes_encrypt, aes_decrypt
 from elliptic_curve import *
 from sha1 import *
+import binascii
 
+
+def print_status(status, text):
+    if status == '+':
+        text = '  '+text
+    print("[{}] {}".format(status, text))
+
+
+# 生成Hash
+message = input("Please input the message you want to send: ")
+print_status('*', "Using SHA1 to generate hash value...")
+message_hash = sha1(message.encode('utf8'))
+print_status("+", "Hash generated: {}".format(message_hash))
+print_status(
+    "+", "Hex form: {}".format(binascii.b2a_hex(sha1(message_hash)).decode('utf8')))
+
+
+# 椭圆曲线签名Hash值
+print_status('*', "Using ECDSA to create a signature...")
+print_status(
+    '*', "  I am using ecp256k1 as the curve. Here are the parameters: (https://www.secg.org/sec2-v2.pdf)")
 # ecp256k1 https://www.secg.org/sec2-v2.pdf
 a = 0
 b = 7
@@ -10,18 +31,71 @@ Gx = '79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798'
 Gy = '483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8'
 n = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'
 p, Gx, Gy, n = [int(num, 16) for num in [p, Gx, Gy, n]]
+print('''
+    a = {}
+    b = {}
+    p = {}
+    G = ({},{})
+    n = {}
+'''.format(a, b, p, Gx, Gy, n)
+      )
 
 ec = EllipticCurve(a, b, p, (Gx, Gy), n)
-pri = ec.get_private_key()
-pub = ec.get_public_key(pri)
+ec_pri = ec.get_private_key()
+print_status('+', '  Private key generated: {}'.format(ec_pri))
+ec_pub = ec.get_public_key(ec_pri)
+print_status('+', '  Public key generated: {}'.format(ec_pub))
 
-data = b'fasdo23dfdfjoo'
-data_hash = sha1(data)
+signature = ec.get_signature(message_hash, ec_pri)
+print_status('+', 'Signature generated: {}'.format(signature))
 
-# signature = ec.sign(data_hash, pri)
+
+# 串接明文和签名
+package = '{}|{},{}'.format(message, *signature)
+print_status(
+    '+', "Concatenated message and signature (package): {}".format(package))
+
+
+# 对称加密package
+print_status('*', 'Using AES128 to encrypt the package...')
+aes_secret = input(
+    "  Please input your secret (Not the key to AES128, Used to generate the key, Any length): ")
+encrypted_package = aes_encrypt(aes_secret, package)
+print_status('+', 'Encrypted package: {}'.format(encrypted_package))
+
+
+# 传输
+print_status('*', 'Transmitting the encrypted package...')
+print_status('+', 'Bleep Bleep Bloop Bloop...')
+print_status('+', '  Local->Remote')
+
+
+# 对称解密package
+print_status('*', 'Using AES128 to decrypt the package...')
+print_status('!', "Your secret is '{}', remember?".format(aes_secret))
+decrypted_package = aes_decrypt(aes_secret, encrypted_package).decode('utf8')
+print_status('+', 'Decrypted package: {}'.format(decrypted_package))
+
+
+# 将明文和签名分开
+print_status('*', 'Spliting message and signature...')
+decrypted_message, decrypted_signature = decrypted_package.split('|')
+decrypted_signature = tuple((int(x) for x in decrypted_signature.split(',')))
+print_status('+', 'Message: {}'.format(decrypted_message))
+print_status('+', 'Signature: {}'.format(decrypted_signature))
+
+
+# 检查是否正确的签名
+print_status('*', "Checking whether the signature is valid")
+print_status(
+    '!', "Your Elliptic curve public key is: {}, remember?".format(ec_pub))
+print_status('+', "Is the signature valid? {}".format(
+    ec.is_valid_signature(sha1(decrypted_message.encode('utf8')), decrypted_signature, ec_pub)))
+
+
 # print(ec.is_valid_signature(data_hash, signature, pub))
 
-cipher = ec.encrypt(data, pub)
-print(cipher)
-plain = ec.decrypt(cipher, pri)
-print(plain)
+# cipher = ec.encrypt(data, ec_pub)
+# print(cipher)
+# plain = ec.decrypt(cipher, ec_pri)
+# print(plain)
